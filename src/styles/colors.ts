852891e0849bd791057cdaae756a873855a1f50d
1Jeferson <jefersonnanet.95@gmail.com>
2024-10-29 11:14:49 -0300

    style: add colors globals

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  background: "#090014",
+  backgroundSecondary: "#27272A",
+  white: "#FFF",
+  textColor: "#CCC",
+
+  button: {
+    blue: "#40DCFC",
+    red: "#E83D55",
+    green: "#00B960",
+  },
+};
